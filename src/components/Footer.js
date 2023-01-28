1e3cab35b7e05321405dbde5a535f7fe70924a7f
Harrison <jiksdevops@gmail.com>
2023-01-28 21:26:58 +0100

    Remove unused CSS classes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,30 +16,15 @@ const Footer = () => {
         })}
       </ul>
       <ul className='footer-icons'>
-        {/* {socialLinks.map((link) => {
+        {socialLinks.map((link) => {
           return (
             <li key={link.id}>
-              <a href={link.href} className='nav-icon' target='_blank' rel='noreferrer'>
+              <a href={link.href} className='footer-icon' target='_blank' rel='noreferrer'>
                 <i className={link.icon}></i>
               </a>
             </li>
           );
-        })} */}
-        <li>
-          <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-            <i className='fab fa-facebook'></i>
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-            <i className='fab fa-twitter'></i>
-          </a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-            <i className='fab fa-squarespace'></i>
-          </a>
-        </li>
+        })}
       </ul>
       <p className='copyright'>
         copyright &copy;Naija Tour {new Date().getFullYear()}. We help you see Nigeria Beauty in 3D.
